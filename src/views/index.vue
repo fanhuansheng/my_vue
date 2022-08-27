e6e8b1a9a2cbebf138915df6b225c63c1ae31b51
fan1140427762 <1140427762@qq.com>
2022-08-28 01:38:05 +0800

    测试

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
 
-    <router-link to="/HelloWorld">HelloWorld</router-link><br/>
-    <router-link to="/demo1">demo1</router-link><br/>
-    <button type="button" @click="goPage('video')">video路由传参</button><br/>
-     <router-link to="/editor">editor</router-link><br/>
-    <router-link to="/map">map</router-link><br/>
-    <router-link to="/upload">upload</router-link><br/>
-    <router-link to="/attrs">$attrs,$listeners</router-link><br/>
-
+    <router-link to="/HelloWorld">HelloWorld</router-link>
+    <br/>
+    <router-link to="/demo1">demo1</router-link>
+    <br/>
+    <button type="button" @click="goPage('video')">video路由传参</button>
+    <br/>
+    <router-link to="/editor">editor</router-link>
+    <br/>
+    <router-link to="/map">map</router-link>
+    <br/>
+    <router-link to="/upload">upload</router-link>
+    <br/>
+    <router-link to="/attrs">$attrs,$listeners</router-link>
+    <br/>
+    <div>测试添加</div>
 
   </div>
 </template>
@@ -17,12 +24,12 @@
 
 export default {
   name: "index",
-  methods:{
-    goPage(params){
+  methods: {
+    goPage(params) {
       this.$router.push({
-        path:'/video',
-        query:{
-          srcUrl:'https://www.huya.com/632956'
+        path: '/video',
+        query: {
+          srcUrl: 'https://www.huya.com/632956'
         }
       })
 
