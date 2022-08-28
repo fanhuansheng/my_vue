0f12ba3612c81af771f207305b91d4144e695064
fan1140427762 <1140427762@qq.com>
2022-08-28 11:02:57 +0800

    添加布局

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,47 @@
 <template>
   <div>
+    <el-container>
+      <el-header style="padding: 10px">
+        <el-row :gutter="20">
+          <el-col :span="16">
+            <div class="grid-content bg-purple">个人网址</div>
+          </el-col>
+          <el-col :span="4">
+            <div class="bg-purple">
+              <el-button type="primary">登陆</el-button>
+            </div>
+          </el-col>
+          <el-col :span="4">
+            <div class="bg-purple">
+              <el-button type="danger">注销</el-button>
+            </div>
+          </el-col>
 
-    <router-link to="/HelloWorld">HelloWorld</router-link><br/>
-    <router-link to="/demo1">demo1</router-link><br/>
-    <button type="button" @click="goPage('video')">video路由传参</button><br/>
-     <router-link to="/editor">editor</router-link><br/>
-    <router-link to="/map">map</router-link><br/>
-    <router-link to="/upload">upload</router-link><br/>
-    <router-link to="/attrs">$attrs,$listeners</router-link><br/>
+        </el-row>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+        </el-aside>
+        <el-main>
+          <div>
+            <router-link to="/HelloWorld">HelloWorld</router-link>
+            <br/>
+            <router-link to="/demo1">demo1</router-link>
+            <br/>
+            <button type="button" @click="goPage('video')">video路由传参</button>
+            <br/>
+            <router-link to="/editor">editor</router-link>
+            <br/>
+            <router-link to="/map">map</router-link>
+            <br/>
+            <router-link to="/upload">upload</router-link>
+            <br/>
+            <router-link to="/attrs">$attrs,$listeners</router-link>
+            <br/>
+          </div>
+        </el-main>
+      </el-container>
+    </el-container>
 
 
   </div>
@@ -17,12 +51,12 @@
 
 export default {
   name: "index",
-  methods:{
-    goPage(params){
+  methods: {
+    goPage(params) {
       this.$router.push({
-        path:'/video',
-        query:{
-          srcUrl:'https://www.huya.com/632956'
+        path: '/video',
+        query: {
+          srcUrl: 'https://www.huya.com/632956'
         }
       })
 
@@ -32,5 +66,7 @@ export default {
 </script>
 
 <style scoped>
-
+.grid-content {
+  background: #cccccc;
+}
 </style>
